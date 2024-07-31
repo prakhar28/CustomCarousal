c8fd0d4d972c72beb580448769e6510ea9fabda5
prakhar28 <pajain96@gmail.com>
2024-07-31 14:51:54 -0400

    data updates

--- src/hooks/CarouselContext.js
+++ src/hooks/CarouselContext.js
@@ -5,10 +5,26 @@ const CarouselContext = createContext();
 export const CarouselProvider = ({children}) => {
   const [currentStep, setCurrentStep] = useState(0);
   const steps = [
-    {name: 'Update Consent', key: 'update_consent'},
-    {name: 'Update Mobile', key: 'update_mobile'},
-    {name: 'Paperless Enroll', key: 'paperless_enroll'},
-    {name: 'Recurring Payment', key: 'recurring_payment'},
+    {
+      name: 'Update Consent',
+      key: 'update_consent',
+      description: 'Some description',
+    },
+    {
+      name: 'Update Mobile',
+      key: 'update_mobile',
+      description: 'Some description',
+    },
+    {
+      name: 'Paperless Enroll',
+      key: 'paperless_enroll',
+      description: 'Some description',
+    },
+    {
+      name: 'Recurring Payment',
+      key: 'recurring_payment',
+      description: 'Some description',
+    },
   ];
   return (
     <CarouselContext.Provider value={{currentStep, setCurrentStep, steps}}>
